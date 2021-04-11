e3982a013556219d2c794cc415cd840ad28c2381
Suri Peng <peng.siyu@hotmail.com>
2021-04-10 18:17:35 -0700

    add swagger config for documentaion

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -4,13 +4,42 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.udacity.pricing.domain.price.PriceRepository;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
+	@Autowired
+	PriceRepository priceRepository;
 
+	@Autowired
+	private MockMvc mockMvc;
+	
 	@Test
-	public void contextLoads() {
+	public void getPriceById() throws Exception{
+		mockMvc.perform(get("/prices/1"))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.vehicleId").exists())
+		.andExpect(jsonPath("$.vehicleId").value(1));
+	}
+	
+	@Test
+	public void getAllPrices() throws Exception{
+		mockMvc.perform(get("/prices/"))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$._embedded").exists())
+		.andExpect(jsonPath("$._embedded.prices").exists())
+		.andExpect(jsonPath("$._embedded.prices", hasSize(19)));
 	}
 
 }
